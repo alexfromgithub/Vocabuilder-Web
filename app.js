4d7a394e1ec99088d2ae3e92739c900ae102db74
aliu0 <mingzhangliu@gmail.com>
2018-02-19 20:59:14 -0500

    Layout completed
    Includes a navbar with title on middle, main links on left and account/logout on right.
    Able to use "include" to set layout on new page.

--- app.js
+++ app.js
@@ -4,7 +4,12 @@ var app = express();
 
 app.set('view engine', 'ejs');
 
+app.use('/public', express.static('public'));
+app.use('/node_modules', express.static('node_modules'));
 app.get('/', function(req, res){
-  res.send('Welcome to Vocabulary Builder');
-})
+  res.render('index', {title: 'Home'});
+});
+app.get('/word-list', function(req, res){
+  res.render('wordlist', {title: 'Word List'});
+});
 app.listen(3000);
