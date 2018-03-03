1f3fc5776ab815e4f9c9cc47c3581114790379ea
alexfromgithub <mingzhangliu@gmail.com>
2018-03-03 08:36:59 -0500

    Change name and email works. Does not allow changing password if old password is incorrect

--- app/routes.js
+++ app/routes.js
@@ -51,6 +51,7 @@ module.exports = function(app, passport) {
     if (req.isAuthenticated()){
       res.render('profile', {
         title: 'Profile',
+        message: req.flash('message'),
         user: req.user
       });
     }
@@ -60,8 +61,25 @@ module.exports = function(app, passport) {
   });
 
   app.post('/changename', function(req, res) {
-    console.log(req.body.firstname);
+    changeName(req.user.id, req.body.firstname, req.body.lastname);
     res.redirect('profile');
+  });
+
+  app.post('/changeemail', function(req, res) {
+    changeEmail(req.user.id, req.body.email);
+    res.redirect('profile');
+  });
+
+  app.post('/changepw', function(req, res, next) {
+    connection.query("SELECT * FROM users WHERE id = ?", [req.user.id], function(err, rows) {
+      if (err) throw err;
+      if (req.body.oldpw !== rows[0].password) {
+        console.log("dd");
+        req.flash('message', 'Sorry. Your old password is incorrect.');
+        res.redirect('profile');
+      }
+    });
+    // res.redirect('profile');
   });
 
   app.get('/word-list', function(req, res) {
@@ -82,8 +100,32 @@ module.exports = function(app, passport) {
   });
 };
 
+var mysql = require('mysql');
+var dbconfig = require('../config/database');
+var bcrypt = require('bcrypt-nodejs');
+var connection = mysql.createConnection(dbconfig.connection);
+connection.query('USE ' + dbconfig.database);
+
 function isLoggedIn(req, res, next) {
   if (req.isAuthenticated())
     return next();
   res.redirect('/');
+}
+
+function changeName(id, firstname, lastname) {
+  connection.query("UPDATE users SET firstname = ?, lastname = ? WHERE id = ?",
+    [firstname,lastname,id], function(err, rows){
+    if (err) throw err;
+  });
+}
+
+function changeEmail(id, email) {
+  connection.query("UPDATE users SET email = ? WHERE id = ?",
+    [email,id], function(err, rows){
+    if (err) throw err;
+  });
+}
+
+function changePW(id, oldpw, newpw, newpw2, done, req) {
+
 }
